70cdefeec33604f92d6d53ee9b6e39f5a4277a3c
livias1726 <livia.simoncini@alumni.uniroma2.eu>
2023-02-27 13:01:26 +0100

    cuda init

--- cuda/cu_spmm.cu
+++ cuda/cu_spmm.cu
@@ -3,7 +3,7 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 #include <helper_timer.h>
-#include "../utils/utils.h"
+#include "cu_utils.h"
 
 // Simple 1-D thread block
 #define BD 256
@@ -11,7 +11,7 @@
 const dim3 BLOCK_DIM(BD);
 
 // GPU implementation of matrix_vector product using a block of threads for each row.
-__global__ void product_csr(int rows, int cols, const float* A, const float* x, float* y) {
+__global__ void gpuProductCsr(int rows, int cols, const float* A, const float* x, float* y) {
     // use of shared memory
     extern __shared__ float shared_row[];
 
@@ -47,70 +47,49 @@ int main(int argc, char** argv) {
     FILE *f;
     CSR* csr;
     ELL* ell;
-    double *x, *y;
-    int k, m, n, nz, num_threads;
-    struct timespec t1, t2;
-    bool ellpack;
+    double gflops_s, gflops_p, abs_err, rel_err;
+    double *x, *y_s, *y_p;
+    int k, m, n, nz;
 
-    // check the correct use of the program
-    if (argc < 4){
-        fprintf(stderr, "Usage: %s [mm-filename] [storage-format] [k value]\n", argv[0]);
-        exit(-1);
-    }
-
-    // create file path
-#ifdef PERFORMANCE
-    char path[PATH_MAX] = "resources/files/";
-#else
-    char path[PATH_MAX] = "../resources/files/";
-#endif
-    strcat(path, argv[1]);
-
-    //check the correct opening of the matrix file
-    f = fopen(path, "r");
-    if (f == NULL) {
-        fprintf(stderr, "Cannot open '%s'\n", path);
-        exit(-1);
-    }
-
-    // get k value and desired storage format
-    ellpack = !strcmp(argv[2], "ellpack");
-    k = (int)strtol(argv[3], NULL, 10);
-
-    // process the first line of file and identify the matrix type
-    if (mm_read_banner(f, &t) != 0){
-        printf("Could not process Matrix Market banner.\n");
-        exit(-1);
-    }
-
-    // check matrix type support
-    check_mat_type(t);
+    process_arguments(argc, argv, &f, &k);
+    process_mm(&t, f);
 
     // ----------------------- Host memory initialisation ----------------------- //
 
     // convert to wanted storage format
-    if (ellpack) {
-        ell = read_mm_ell(f, t);
-        m = ell->M;
-        n = ell->N;
-        nz = ell->NZ;
-#ifdef AUDIT
-        print_ell(ell);
+#ifdef ELLPACK
+    ell = read_mm_ell(f, t);
+    m = ell->M;
+    n = ell->N;
+    nz = ell->NZ;
+
+    #ifdef DEBUG
+    print_ell(ell);
+    #endif
+
+    ELL *d_ell;
+    checkCudaErrors(cudaMalloc((void**) &d_ell, sizeof(ELL)));
+    checkCudaErrors(cudaMemcpy(d_ell, ell, sizeof(ELL), cudaMemcpyHostToDevice));
+#else
+    csr = read_mm_csr(f, t);
+    m = csr->M;
+    n = csr->N;
+    nz = csr->NZ;
+
+    #ifdef DEBUG
+    print_csr(csr);
+    #endif
+
+    CSR *d_csr;
+    checkCudaErrors(cudaMalloc((void**) &d_csr, sizeof(CSR)));
+    checkCudaErrors(cudaMemcpy(d_csr, csr, sizeof(CSR), cudaMemcpyHostToDevice));
 #endif
-    } else {
-        csr = read_mm_csr(f, t);
-        m = csr->M;
-        n = csr->N;
-        nz = csr->NZ;
-#ifdef AUDIT
-        print_csr(csr);
-#endif
-    }
 
     fclose(f);
 
     alloc_struct(&x, n, k);
-    alloc_struct(&y, m ,k);
+    alloc_struct(&y_s, m ,k);
+    alloc_struct(&y_p, m ,k);
 
     populate_multivector(x, n, k);
 
@@ -119,71 +98,71 @@ int main(int argc, char** argv) {
     print_matrix(x, n, k, "\nMultivector:\n");
 #endif
 
-// ---------------------- Device memory initialisation ---------------------- //
+    double *d_x, *d_y;
+    checkCudaErrors(cudaMalloc((void**) &d_x, n * k * sizeof(double)));
+    checkCudaErrors(cudaMalloc((void**) &d_y, m * k * sizeof(double)));
 
-    float *d_A, *d_x, *d_y;
+    checkCudaErrors(cudaMemcpy(d_x, x,  n * k * sizeof(double), cudaMemcpyHostToDevice));
 
-    checkCudaErrors(cudaMalloc((void**) &d_A, nrows * ncols * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void**) &d_x, ncols * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void**) &d_y, nrows * sizeof(float)));
 
-    // Copy matrices from the host (CPU) to the device (GPU).
-    checkCudaErrors(cudaMemcpy(d_A, h_A, nrows * ncols * sizeof(float), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_x, h_x,  ncols * sizeof(float), cudaMemcpyHostToDevice));
-
-    // ------------------------ Product on the GPU ------------------------- //
-
-    float flopcnt=2.e-6*nrows*ncols;
-
-    // Create the CUDA SDK timer.
+    // CUDA SDK timer
     StopWatchInterface* timer = 0;
     sdkCreateTimer(&timer);
 
-    // DONE: Calculate the dimension of the grid of blocks (1D) needed to cover all
-    // entries in the matrix and output vector
-    // const dim3 GRID_DIM(ceil((nrows*ncols)/BD));
+    // ------------------------ Product on the CPU ------------------------- //
+    timer->start();
+#ifdef ELLPACK
+    serial_product_ell(ell, x, k, y_s);
+#else
+    serial_product_csr(csr, x, k, y_s);
+#endif
+    timer->stop();
+
+    gflops_s = get_gflops((timer->getTime())*1.e6, k, nz);
+
+    // ------------------------ Product on the GPU ------------------------- //
+    // TODO: compute the dimension of the grid of blocks (1D) needed to cover all entries in the matrix and output vector
     const dim3 GRID_DIM(((nrows*ncols)+BD - 1)/BD);
     const int shmem_size = BD*sizeof(float);
 
     timer->start();
-    gpuMatrixVector<<<GRID_DIM, BLOCK_DIM, shmem_size>>>(nrows, ncols, d_A, d_x, d_y);
+    gpuProductCsr<<<GRID_DIM, BLOCK_DIM, shmem_size>>>(nrows, ncols, d_A, d_x, d_y);
     checkCudaErrors(cudaDeviceSynchronize());
-
     timer->stop();
-    float gpuflops=flopcnt/ timer->getTime();
-    std::cout << "  GPU time: " << timer->getTime() << " ms." << " GFLOPS " << gpuflops<<std::endl;
 
-    // Download the resulting vector d_y from the device and store it in h_y_d.
-    checkCudaErrors(cudaMemcpy(h_y_d, d_y, nrows*sizeof(float),cudaMemcpyDeviceToHost));
+    gflops_p = get_gflops((timer->getTime())*1.e6, k, nz);
 
-    // Now let's check if the results are the same.
-    float reldiff = 0.0f;
-    float diff = 0.0f;
+    // get the resulting vector d_y from the device and store it in y
+    checkCudaErrors(cudaMemcpy(y_p, d_y, m * k * sizeof(double), cudaMemcpyDeviceToHost));
 
-    for (int row = 0; row < nrows; ++row) {
-        float maxabs = std::max(std::abs(h_y[row]),std::abs(h_y_d[row]));
-        if (maxabs == 0.0) {
-            maxabs = 1.0;
-        }
-        reldiff = std::max(reldiff, std::abs(h_y[row] - h_y_d[row])/maxabs);
-        diff = std::max(diff, std::abs(h_y[row] - h_y_d[row]));
-    }
-    std::cout << "Max diff = " << diff << "  Max rel diff = " << reldiff << std::endl;
-    // Rel diff should be as close as possible to unit roundoff;
-    // float corresponds to IEEE single precision, so unit roundoff is 1.19e-07
+    // check results
+    abs_err = get_absolute_error(m*k, y_s, y_p);
+    rel_err = get_relative_error(m*k, abs_err, y_s);
 
 // ------------------------------- Cleaning up ------------------------------ //
-
     delete timer;
 
-    checkCudaErrors(cudaFree(d_A));
+#ifdef ELLPACK
+    checkCudaErrors(cudaFree(d_ell));
+    delete[] ell;
+#else
+    checkCudaErrors(cudaFree(d_csr));
+    delete[] csr;
+#endif
+
     checkCudaErrors(cudaFree(d_x));
     checkCudaErrors(cudaFree(d_y));
 
-    delete[] h_A;
-    delete[] h_x;
-    delete[] h_y;
-    delete[] h_y_d;
+    delete[] x;
+    delete[] y_s;
+    delete[] y_p;
+
+#ifdef PERFORMANCE
+    fprintf(stdout, "%f %f %f %f", gflops_s, gflops_p, abs_err, rel_err);
+#else
+    fprintf(stdout, "\nSerial GFLOPS: %f\nParallel GFLOPS: %f\nAbsolute error: %f\nRelative error: %f\n",
+            gflops_s, gflops_p, abs_err, rel_err);
+#endif
 
     return 0;
 }
---
+++ cuda/cu_utils.h
@@ -0,0 +1,7 @@
+#include "../utils/utils.h"
+
+#ifndef SCPA_PROJECT_CU_UTILS_H
+    #define SCPA_PROJECT_CU_UTILS_H
+#endif
+
+void process_arguments(int argc, char** argv, FILE **f, int* k);
